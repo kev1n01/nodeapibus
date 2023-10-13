eeb18a6aa169fa94704b713c55685766c7476f8c
kevin arnold flores pacheco <arnold_k2001@hotmail.com>
2023-10-13 12:28:07 -0500

    config conexion a psql

--- src/config.js
+++ src/config.js
@@ -3,16 +3,18 @@ import { config } from "dotenv"
 config()
 
 const PORT = process.env.PORT
-const DB_HOST = process.env.DB_HOST
-const DB_PORT = process.env.DB_PORT
-const DB_USER = process.env.DB_USER
-const DB_PASSWORD = process.env.DB_PASSWORD
-const DB_DATABASE = process.env.DB_DATABASE
+const DB_URL = process.env.DB_URL
+// const DB_HOST = process.env.DB_HOST
+// const DB_PORT = process.env.DB_PORT
+// const DB_USER = process.env.DB_USER
+// const DB_PASSWORD = process.env.DB_PASSWORD
+// const DB_DATABASE = process.env.DB_DATABASE
 export {
     PORT,
-    DB_HOST,
-    DB_PORT,
-    DB_USER,
-    DB_PASSWORD,
-    DB_DATABASE,
+    DB_URL,
+    // DB_HOST,
+    // DB_PORT,
+    // DB_USER,
+    // DB_PASSWORD,
+    // DB_DATABASE,
 }
--- src/db.js
+++ src/db.js
@@ -1,16 +1,14 @@
 import {
-    DB_HOST,
-    DB_PORT,
-    DB_USER,
-    DB_PASSWORD,
-    DB_DATABASE
+    // DB_HOST,
+    // DB_PORT,
+    // DB_USER,
+    // DB_PASSWORD,
+    // DB_DATABASE
+    DB_URL
 } from '../src/config.js'
 import pg from 'pg'
 
 export const pool = new pg.Pool({
-    host: DB_HOST,
-    user: DB_USER,
-    password: DB_PASSWORD,
-    port: DB_PORT,
-    database: DB_DATABASE
+    connectionString: DB_URL,
+    ssl: true
 })
