fedf31654a72f4bfcdadc576ed841cc256315b28
kevin arnold flores pacheco <arnold_k2001@hotmail.com>
2024-01-02 13:40:19 -0500

    speed added

--- src/db.js
+++ src/db.js
@@ -1,16 +1,16 @@
 import {
-    DB_HOST,
-    DB_PORT,
-    DB_USER,
-    DB_PASSWORD,
-    DB_DATABASE,
+    // DB_HOST,
+    // DB_PORT,
+    // DB_USER,
+    // DB_PASSWORD,
+    // DB_DATABASE,
     DB_URL
 } from '../src/config.js'
 import pg from 'pg'
 
 export const pool = new pg.Pool({
     connectionString: DB_URL,
-    // ssl: true,
+    ssl: true,
     // hostname: DB_HOST,
     // user: DB_USER,
     // password: DB_PASSWORD,
